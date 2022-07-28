fb6a0d1ce42cf4dfa78d0111c9e8eb54cad3ecfd
Jon Tindal <jon.tindal@outlook.com>
2022-07-28 22:13:23 +0100

    Start of back prop

--- neural_network.cpp
+++ neural_network.cpp
@@ -2,35 +2,34 @@
 
 #include <cassert>
 #include <algorithm>
-#include <functional>
 #include <numeric>
 
 NeuralNetwork::NeuralNetwork(activation_func_t activation_func, activation_func_deriv_t activation_func_deriv,
-                             std::vector<size_t> hidden_layer_sizes, int alpha):
+                             std::vector<size_t> hidden_layer_sizes, double alpha):
                              number_layers(hidden_layer_sizes.size() + 1),
                              alpha(alpha),
                              hidden_layer_sizes(hidden_layer_sizes) {
         
-        this->activation_func = activation_func;
-        this->activation_func_deriv = activation_func_deriv;
+    this->activation_func = activation_func;
+    this->activation_func_deriv = activation_func_deriv;
 
-        std::vector<size_t> total_layers = this->hidden_layer_sizes;
-        total_layers.insert(total_layers.begin(), INPUT_SIZE);
-        total_layers.insert(total_layers.end(), OUTPUT_SIZE);
+    std::vector<size_t> total_layers = this->hidden_layer_sizes;
+    total_layers.insert(total_layers.begin(), INPUT_SIZE);
+    total_layers.insert(total_layers.end(), OUTPUT_SIZE);
 
-        for (unsigned int i = 0; i < total_layers.size() - 1; i++) {
-            std::vector<std::vector<double>> weights_matrix(total_layers[i + 1], std::vector<double>(total_layers[i], 0));
-            weights.push_back(weights_matrix);
+    for (unsigned int i = 0; i < total_layers.size() - 1; i++) {
+        std::vector<std::vector<double>> weights_matrix(total_layers[i + 1], std::vector<double>(total_layers[i], 0));
+        weights.push_back(weights_matrix);
 
-            std::vector<double> biases_array(total_layers[i + 1], 0);
-            biases.push_back(biases_array);
-        }
+        std::vector<double> biases_array(total_layers[i + 1], 0);
+        biases.push_back(biases_array);
     }
+}
 
-neural_net_output_t NeuralNetwork::forward_prop(const std::vector<double>& input_pixels) {
+forward_prop_output_t NeuralNetwork::forward_prop(const std::vector<double>& input_pixels) {
     assert (input_pixels.size() == INPUT_SIZE);
 
-    neural_net_output_t result;
+    forward_prop_output_t result;
     result.z_results.push_back(std::vector<double>());
     result.a_results.push_back(input_pixels);
 
@@ -53,7 +52,7 @@ neural_net_output_t NeuralNetwork::forward_prop(const std::vector<double>& input
     return result;
 }
 
-std::vector<double> cost_function_deriv(unsigned char expected_result, const std::vector<double>& actual_results) {
+std::vector<double> NeuralNetwork::cost_function_deriv(unsigned char expected_result, const std::vector<double>& actual_results) {
     assert (expected_result < actual_results.size());
 
     std::vector<double> one_hot_expected_results(actual_results.size(), 0);
@@ -66,4 +65,19 @@ std::vector<double> cost_function_deriv(unsigned char expected_result, const std
                    [](double val_0, double val_1) { return 2 * (val_0 - val_1); });
 
     return results;
+}
+
+back_prop_output_t NeuralNetwork::back_prop(unsigned char expected_result, const std::vector<double>& input_pixels,
+                                            const forward_prop_output_t& forward_prop_output) {
+
+    std::vector<std::vector<double>> dz_results;
+    dz_results.push_back(cost_function_deriv(expected_result, actual_results));
+
+    for (unsigned int i = 0; i < number_layers - 1; i++) {
+        std::vector<double> dz_result(forward_prop_output.dz_results[i].size(), 0);
+        dz_results.push_back(dz_result);
+    }
+
+    back_prop_output_t result;
+    return result;
 }
--- neural_network.hpp
+++ neural_network.hpp
@@ -10,23 +10,31 @@ constexpr size_t INPUT_SIZE = 784;
 constexpr size_t LAYER_ONE_SIZE = 10;
 constexpr size_t OUTPUT_SIZE = 10;
 
-struct neural_net_output_t {
+struct forward_prop_output_t {
     std::vector<std::vector<double>> z_results;
     std::vector<std::vector<double>> a_results;
 };
 
+struct back_prop_output_t {
+    std::vector<std::vector<std::vector<double>>> dw_results;
+    std::vector<std::vector<double>> db_results;
+}
+
 class NeuralNetwork {
     public:
         NeuralNetwork(activation_func_t activation_func, activation_func_deriv_t activation_func_deriv,
-                      std::vector<size_t> hidden_layer_sizes, int alpha);
+                      std::vector<size_t> hidden_layer_sizes, double alpha);
 
-        neural_net_output_t forward_prop(const std::vector<double>& input_pixels);
+        forward_prop_output_t forward_prop(const std::vector<double>& input_pixels);
 
         std::vector<double> cost_function_deriv(unsigned char expected_result, const std::vector<double>& actual_results);
 
+        back_prop_output_t back_prop(unsigned char expected_result, const std::vector<double>& input_pixels,
+                                     const forward_prop_output_t& forward_prop_output);
+
     private:
         const int number_layers;
-        const int alpha;
+        const double alpha;
 
         const std::vector<size_t> hidden_layer_sizes;
 
