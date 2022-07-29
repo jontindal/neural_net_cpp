c49cca9faa54e8dee921320f255d0026354cc73a
Jon Tindal <jon.tindal@outlook.com>
2022-07-29 20:19:39 +0100

    Make back prop test function for one layer

--- test_funcs.cpp
+++ test_funcs.cpp
@@ -32,3 +32,34 @@ forward_prop_output_t forward_prop(std::vector<std::vector<std::vector<double>>>
 
     return result;
 }
+
+back_prop_output_t back_prop(unsigned char expected_result, forward_prop_output_t forward_prop_output,
+                             std::vector<std::vector<std::vector<double>>> weights) {
+
+    std::vector<double> expected_results(OUTPUT_SIZE, 0);
+    expected_results[expected_result] = 1;
+
+    back_prop_output_t result;
+
+    std::vector<double> dz1(OUTPUT_SIZE, 0);
+
+    for (size_t i = 0; i < OUTPUT_SIZE; i++) {
+        dz1[i] = 2 * (expected_results[i] - forward_prop_output.a_results.back()[i]);
+    }
+
+    std::vector<std::vector<double>> dw1 = weights.back();
+
+    for (size_t i = 0; i < dw1.size(); i++) {
+        for (size_t j = 0; j < dw1[i].size(); j++) {
+            dw1[i][j] = dz1[j] * forward_prop_output.a_results.front()[j];
+        }
+    }
+
+    std::vector<double> db1 = dz1;
+
+    
+    result.dw_results.push_back(dw1);
+    result.db_results.push_back(db1);
+
+    return result;
+}
--- test_funcs.hpp
+++ test_funcs.hpp
@@ -7,3 +7,6 @@ std::vector<double> get_input_pixels();
 forward_prop_output_t forward_prop(std::vector<std::vector<std::vector<double>>> weights,
                                    std::vector<std::vector<double>> biases,
                                    std::vector<double> input_pixels);
+
+back_prop_output_t back_prop(unsigned char expected_result, forward_prop_output_t forward_prop_output,
+                             std::vector<std::vector<std::vector<double>>> weights);
